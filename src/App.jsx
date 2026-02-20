d2926677783ad9bffe0b1db0d6dd56cba61e3287
Stormbringer <nightshade7703@proton.me>
2026-02-20 11:21:17 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecognition />
+    </div>
+  );
+}
+
+export default App;
