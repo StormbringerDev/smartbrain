1e749fff2fb86ab7e2b3a91a8f7c5a4c9273ce3e
Stormbringer <nightshade7703@proton.me>
2026-02-20 13:24:15 -0600

    Create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import ParticlesBg from 'particles-bg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
-      <FaceRecognition />
+      {/* {<FaceRecognition />} */}
     </div>
   );
 }
